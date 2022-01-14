d1f832e57e107c1deeeba4c4d4825c461391cd2e
Alex Carmody <alex-carmody@live.com>
2022-01-14 13:57:16 -0500

    Added menu item scss and js for transition

--- js/main.js
+++ js/main.js
@@ -3,6 +3,7 @@ const hamburger = document.querySelector
 ('.menu-btn__burger');
 const nav = document.querySelector('.nav');
 const menuNav = document.querySelector('.menu-nav');
+const navItems = document.querySelectorAll('.menu-nav__item');
 
 let showMenu = false;
 
@@ -13,12 +14,14 @@ function toggleMenu() {
         hamburger.classList.add('open');
         nav.classList.add('open');
         menuNav.classList.add('open');
+        navItems.forEach(item => item.classList.add('open'));
 
         showMenu = true;
     } else {
         hamburger.classList.remove('open');
         nav.classList.remove('open');
         menuNav.classList.remove('open');
+        navItems.forEach(item => item.classList.remove('open'));
 
         showMenu = false;
     }
